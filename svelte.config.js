fbea7ee91f0dc18a7987414bae3a38dbe1d0af2f
Marcus Fredriksson <marcus@marcusfredriksson.com>
2023-01-21 12:26:44 +0100

    Vercel- and svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,20 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			edge: true,
+			external: [],
+			split: false
+		}),
+		alias: {
+			'$utils/*': 'src/utils/*',
+			'$components/*': 'src/components/*',
+			'$sections/*': 'src/sections/*',
+			'$data/*': 'static/data/*'
+		},
+		csrf: {
+			checkOrigin: !process.env.NODE_ENV === 'development'
+		}
 	}
 };
 
