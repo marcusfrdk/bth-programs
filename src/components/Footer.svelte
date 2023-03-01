11adbd2f7b8dae64fd2d438c800e4017cd8df64d
Marcus Fredriksson <marcus@marcusfredriksson.com>
2023-03-01 21:36:25 +0100

    Updated footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,7 +8,7 @@
   <div>
     <p class="created-by">Skapad av <a href="https://marcusfredriksson.com">marcusfrdk</a></p>
     <p class="generated-at">{program?.generated ? `Datan genererades ${new Date(program?.generated)}` : "Laddar data..."}</p>
-    <p class="note">Denna sidan är inte en officiell del av <a href="https://www.bth.se">BTH</a> och är endast ett hobbyprojekt för att lösa ett problem.</p>
+    <p class="note">Denna sidan är inte en officiell del av <a href="https://www.bth.se">BTH</a> och är endast ett hobbyprojekt.</p>
   </div>
 </footer>
 
@@ -30,14 +30,15 @@
         font-size: 0.875rem;
         color: var(--weak);
         margin-top: 0.25rem;
+        margin-bottom: 0.5rem;
       }
       & > p.note {
-        margin-top: 0.5rem;
         padding: 0.5rem;
         background-color: var(--bottom);
         width: fit-content;
         border-radius: 0.5rem;
         color: var(--muted);
+        font-size: 0.875rem;
         & > a {
           color: var(--muted);
           text-decoration: underline;
