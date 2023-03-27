3cae8b1b693e8e18c4c0524d520ce55c6e111c25
Marcus Fredriksson <marcus@marcusfredriksson.com>
2023-03-27 13:58:45 +0200

    Removed duplicate courses in courses

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,7 +1,20 @@
-import type { ICourse } from "../types/Program";
-import { getNumberOfWeeks } from "./time";
+import type { ICourse } from '../types/Program';
+import { getNumberOfWeeks } from './time';
 
-export const clusterWeeks = (obj: Record<string, any>, maxDiff: number): Record<string, ICourse[]> => {
+const removeDuplicates = (obj: Record<string, ICourse[]>): Record<string, ICourse[]> => {
+	const keys = Object.keys(obj);
+	for (let i = 0; i < keys.length; i++) {
+		const key = keys[i];
+		const unique = Array.from(new Set(obj[key].map((course) => course.code)));
+		obj[key] = unique.map((code) => obj[key].find((course) => course.code === code) as ICourse);
+	}
+	return obj;
+};
+
+export const clusterWeeks = (
+	obj: Record<string, ICourse[]>,
+	maxDiff: number
+): Record<string, ICourse[]> => {
 	const entries = Object.entries(obj);
 	for (let i = 0; i < entries.length - 1; i++) {
 		for (let j = i + 1; j < entries.length; j++) {
@@ -19,30 +32,34 @@ export const clusterWeeks = (obj: Record<string, any>, maxDiff: number): Record<
 			}
 		}
 	}
-	return obj;
+	return removeDuplicates(obj);
 };
 
 export const groupSemesters = (arr: ICourse[]): Record<string, ICourse[]> => {
 	const grouped: Record<string, ICourse[]> = {};
-	const index = Array.from(new Set(arr.map((course) => {
-		grouped[course.start] = [];
-		return course.start;
-	}))).sort();
-	
-	for(let i=0;i<arr.length;i++){
+	const index = Array.from(
+		new Set(
+			arr.map((course) => {
+				grouped[course.start] = [];
+				return course.start;
+			})
+		)
+	).sort();
+
+	for (let i = 0; i < arr.length; i++) {
 		const course = arr[i];
 		const weeks = getNumberOfWeeks(course.start, course.end);
 		let nextSemester = index[index.indexOf(course.start) + 1] || course.start;
-		const currentWeek = Number(course.start.split(" ")[2]);
-		const nextWeek = Number(nextSemester.split(" ")[2] || 0);
-		
+		const currentWeek = Number(course.start.split(' ')[2]);
+		const nextWeek = Number(nextSemester.split(' ')[2] || 0);
+
 		// Add double courses to next semester
-		if(nextWeek - currentWeek < 5) nextSemester = index[index.indexOf(course.start) + 2] || course.start;
-		if (weeks > 10 && typeof nextSemester !== 'undefined') grouped[nextSemester].push(structuredClone(course));
+		if (nextWeek - currentWeek < 5)
+			nextSemester = index[index.indexOf(course.start) + 2] || course.start;
+		if (weeks > 10 && typeof nextSemester !== 'undefined')
+			grouped[nextSemester].push(structuredClone(course));
 		grouped[course.start].push(course);
-	};
+	}
 
-  return clusterWeeks(grouped, 5);
-} 
-
-
+	return clusterWeeks(grouped, 5);
+};
